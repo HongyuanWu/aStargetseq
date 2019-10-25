c262067ab6b4d5728a9e46e0513ae673b23f69e7
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-10-24 19:37:32 -0500

    Add files via upload

---
+++ result/rvtest/qqplot.R
@@ -0,0 +1,14 @@
+args = commandArgs(trailingOnly=TRUE)
+library("Haplin")
+input=args[1]
+Pcol=as.numeric(args[2])
+data=read.table(input,head=T,sep="")
+pvalues=as.numeric(data[,Pcol])
+qqplot<-function(pvalues,output=paste(input,"qqplot.pdf",sep="")){
+pdf(output)
+  pQQ(pvalues, nlabs =length(pvalues), conf = 0.9) 
+dev.off()
+}
+qqplot(pvalues)
+# wget https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/qqplot.R -O qqplot.R
+# Rscript manhattan.plot.R Schrodi_IL23_IL17.Kbac.assoc 4 Schrodi_IL23_IL17.Kbac.assoc
